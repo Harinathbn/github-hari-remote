6d1d191d601a4bbbea92fa97341acde7c125d59f
Harinath_b <harinath_b@persistent.com>
2022-06-07 17:03:52 +0530

    Added fn hedders

--- math.py
+++ math.py
@@ -1,11 +1,15 @@
+# add implementation
 add (a,b):
    pass
-  
+
+# sub implementation  
 sub (a,b):
    pass
-  
+
+# mul implementation  
 mul (a,b):
    pass
-   
+ 
+# div implementation 
 div (a,b):
    pass
